c29d93cd98c7ca87860af64da22e6e4e051f100d
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-10-17 15:48:17 -0400

    UPDATED

--- runaround.js
+++ runaround.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   })
 
   
-	// var mapURL = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA9O_Kfl3FvK2OwNOA8qrLKpJ7wVTycGms`
+
 	var mapURL = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`
 	var map;
 
@@ -26,6 +26,9 @@ $(document).ready(function(){
 
   
  function initMap() {
+  
+
+
   var markerArray = []
         var uluru = {
             lat: 33.9304,
@@ -41,6 +44,78 @@ $(document).ready(function(){
             map: map
         });
     
+
+
+
+
+
+
+
+            var init_lat = 33.7490
+            var init_lng = -84.3880
+            var range = 0.01
+
+      function findCoordinates(lat, lng, range){
+          var numOfPoints = 2;
+
+          var degreesPerPoint = -2 /numOfPoints;
+          var currentAngle = 90;
+          var x2;
+          var y2;
+          // var points = new Array();
+          // console.log(points)
+        for(var i=0; i < numOfPoints; i++){
+          x2 = Math.cos(currentAngle) * range;
+          y2 = Math.sin(currentAngle) * range;
+          newLat = lat+x2;
+          newLng = lng+y2;
+          var lat_lng = new google.maps.LatLng(newLat,newLng);
+          var marker = new google.maps.Marker({
+            position: lat_lng,
+            map: map
+          });
+
+          currentAngle += degreesPerPoint;
+        }
+      }
+
+      function initialize(){
+        var mapDiv = document.getElementById('#map');
+        var myOptions = {
+          zoom: 13,
+          center:lat_lng,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        
+        // var map = new google.maps.Map(mapDiv, {
+        //   center: new google.maps.LatLng(init_lat, init_lng),
+        //   zoom: 13,
+        //   MapTypeId: google.maps.MapTypeId.ROADMAP
+        // });
+
+        var lat_lng = new google.maps.LatLng(init_lat, init_lng);
+        var marker = new google.maps.Marker({
+          position: lat_lng,
+          map: map
+        });
+
+        findCoordinates(init_lat, init_lng, range);
+
+      }
+
+
+
+       google.maps.event.addDomListener(window, 'load', initialize)
+
+
+
+
+
+
+
+
+
 
             // Try HTML5 geolocation.
         if (navigator.geolocation) {
@@ -72,37 +147,189 @@ $(document).ready(function(){
       }
 
 
-      // var directionsService = new google.maps.DirectionsService ;
-      // var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
-      // var stepDisplay = new google.maps.InfoWindow;
+      var directionsService = new google.maps.DirectionsService;
+      console.log(directionsService)
 
-      // calculateAndDislayRoute(
-      //   directionsDisplay, directionsService, markerArray, stepDisplay, map);
+      var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+      console.log(directionsDisplay)
+      var stepDisplay = new google.maps.InfoWindow;
 
-      // var onChangeHandler = function(){
-      //   calculateAndDislayRoute(
-      //   directionsDisplay, directionsService, markerArray, stepDisplay, map);
-      // };
+      calculateAndDislayRoute(
+        directionsDisplay, directionsService, markerArray, stepDisplay, map);
 
-      // document.getElementById('start').addEventListener('change', onChangeHandler);
-      // document.getElementById('end').addEventListener('change', onChangeHandler);
+      var onChangeHandler = function(){
+        calculateAndDislayRoute(
+        directionsDisplay, directionsService, markerArray, stepDisplay, map);
+      };
 
-      // function calculateAndDislayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map){
-      //   for(var i = 0; i < markerArray.length; i++){
-      //     markerArray[i].setMap(null);
-      //   }
+      document.getElementById('start').addEventListener('change', onChangeHandler);
+      document.getElementById('end').addEventListener('change', onChangeHandler);
+      document.getElementById('startend').addEventListener('change', onChangeHandler);
 
-      //   directionsService.route({
-      //     origin: document.getElementById('start').value;
-      //     destination: document.getElementById('end').value;
-      //     travelMode: WALKING;
 
-      //   }function(response,status){
-          
-      //   })
-      // }
+      function calculateAndDislayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map){
+        // console.log("test")
+        for(var i = 0; i < markerArray.length; i++){
+          markerArray[i].setMap(null);
+        }
+        // console.log("test")
+                  // console.log(directionsService.route)
+                  var start = document.getElementById('start').value
+                  var end =  document.getElementById('end').value
+                  var startend = document.getElementById('startend').value
+        directionsService.route({
+          origin: start,
+          destination: end,
+          travelMode: "WALKING",
+          optimizeWaypoints: false
+        }, check
+        )
+    // Route the directions and pass the response to a function to create)
+        function check (response,status) {
+          if(status === "OK"){
+            document.getElementById('warning-panel').innerHTML = '<b>' + response.routes[0].warnings + '</b>';
+            directionsDisplay.setDirections(response);
+            showSteps(response,markerArray, stepDisplay, map);
 
+          }else{
+            window.alert("Request failed due to" + status)
+          }
+        }
+      }
+      console.log("test")
+
+    
+      var myRoute;
+    function showSteps(directionResult, markerArray, stepDisplay, map){
+      myRoute = directionResult.routes[0].legs[0];
+      for(var i = 0; i < myRoute.steps.length; i++){
+        var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+        marker.setMap(map);
+        marker.setPosition(myRoute.steps[i].start_location);
+        attachInstructionText(
+          stepDisplay, marker, myRoute.steps[i].instructions.map);
+      }
     }
+
+    function attachInstructionText(stepDisplay, marker, text, map){
+      google.maps.event.addListener(marker, "click", function(){
+        stepDisplay.setContent(text);
+        stepDisplay.open(map, marker)
+      })
+    }
+     var circle = new google.maps.Circle({
+      map:map,
+      center: uluru,
+      radius: 3218,
+      fillColor: '#ff0000'
+      });
+    
+
+
+
+
+    // init_lat = 33.7490
+    // init_lng = -84.3880
+    // range = 0.3
+
+    //   function findCoordinates(lat, lon, range){
+    //       var numOfPoints = 2;
+    //       var degreesPerPoint = 100/numOfPoints;
+    //       var currentAngle = 90;
+    //       var x2;
+    //       var y2;
+    //       var points = new Array();
+    //     for(var i=0; i < numOfPoints; i++){
+    //       x2 = Math.cos(currentAngle) * range;
+    //       y2 = Math.sin(currentAngle) * range;
+    //       newLat = lat+x2;
+    //       newLng = lon+y2;
+    //       var lat_lng = new google.maps.LatLng(newLat,newLng);
+    //       var marker = new google.maps.Marker({
+    //         position: lat_lng,
+    //         map: map
+    //       });
+
+    //       currentAngle += degreesPerPoint;
+    //     }
+    //   }
+
+    //   function initialize(){
+    //     var mapDiv = document.getElementById('#map');
+    //     var map = new google.maps.Map(mapDiv, {
+    //       center: new google.maps.LatLng(init_lat, init_lng),
+    //       zoom: 13,
+    //       MapTypeId: google.maps.MapTypeId.ROADMAP
+    //     });
+
+    //     var lat_lng = new google.maps.LatLng(init_lat, init_lng);
+    //     var marker = new google.maps.Marker({
+    //       position: lat_lng,
+    //       map: map
+    //     });
+
+    //     findCoordinates(init_lat, init_lng, range);
+
+    //   }
+
+    //    google.maps.event.addDomListener(window, 'load', initialize)
+      
+}
+
+
+    // init_lat = 33.7490
+    // init_lng = -84.3880
+    // range = 0.3
+
+    //   function findCoordinates(lat, lon, range){
+    //       var numOfPoints = 2;
+    //       var degreesPerPoint = 100/numOfPoints;
+    //       var currentAngle = 90;
+    //       var x2;
+    //       var y2;
+    //       var points = new Array();
+    //     for(var i=0; i < numOfPoints; i++){
+    //       x2 = Math.cos(currentAngle) * range;
+    //       y2 = Math.sin(currentAngle) * range;
+    //       newLat = lat+x2;
+    //       newLng = lon+y2;
+    //       var lat_lng = new google.maps.LatLng(newLat,newLng);
+    //       var marker = new google.maps.Marker({
+    //         position: lat_lng,
+    //         map: map
+    //       });
+
+    //       currentAngle += degreesPerPoint;
+    //     }
+    //   }
+
+    //   function initialize(){
+    //     var mapDiv = document.getElementById('#map');
+    //     map = new google.maps.Map(mapDiv, {
+    //       center: new google.maps.LatLng(init_lat, init_lng),
+    //       zoom: 13,
+    //       MapTypeId: google.maps.MapTypeId.ROADMAP
+    //     });
+
+    //     var lat_lng = new google.maps.LatLng(init_lat, init_lng);
+    //     var marker = new google.maps.Marker({
+    //       position: lat_lng,
+    //       map: map
+    //     });
+
+    //     findCoordinates(init_lat, init_lng, range);
+
+    //   }
+
+      // google.maps.event.addDomListener(window, 'load', initialize)
+
+
+
+
+
+  // var position = new google.maps.LatLng()
+
+ 
 
 
   // $(window).bind('scroll', function() {
@@ -114,9 +341,9 @@ $(document).ready(function(){
   //    }
   // });
 
-  setTimeout(function(){
-    $("#myModal").modal('show')
-  }, 1000)
+  // setTimeout(function(){
+  //   $("#myModal").modal('show')
+  // }, 1000)
 
   
 
