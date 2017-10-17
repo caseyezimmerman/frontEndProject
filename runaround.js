2663586a95151c614122c6c62744741f8c17b7c3
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-10-17 11:24:03 -0400

    updated

--- runaround.js
+++ runaround.js
@@ -26,6 +26,7 @@ $(document).ready(function(){
 
   
  function initMap() {
+  var markerArray = []
         var uluru = {
             lat: 33.9304,
             lng: -84.3733
@@ -50,7 +51,7 @@ $(document).ready(function(){
             };
             var infoWindow = new google.maps.InfoWindow()
             infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
+            infoWindow.setContent('Current Location');
             infoWindow.open(map);
             map.setCenter(pos);
           }, function() {
@@ -69,17 +70,49 @@ $(document).ready(function(){
                               'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
       }
+
+
+      // var directionsService = new google.maps.DirectionsService ;
+      // var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+      // var stepDisplay = new google.maps.InfoWindow;
+
+      // calculateAndDislayRoute(
+      //   directionsDisplay, directionsService, markerArray, stepDisplay, map);
+
+      // var onChangeHandler = function(){
+      //   calculateAndDislayRoute(
+      //   directionsDisplay, directionsService, markerArray, stepDisplay, map);
+      // };
+
+      // document.getElementById('start').addEventListener('change', onChangeHandler);
+      // document.getElementById('end').addEventListener('change', onChangeHandler);
+
+      // function calculateAndDislayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map){
+      //   for(var i = 0; i < markerArray.length; i++){
+      //     markerArray[i].setMap(null);
+      //   }
+
+      //   directionsService.route({
+      //     origin: document.getElementById('start').value;
+      //     destination: document.getElementById('end').value;
+      //     travelMode: WALKING;
+
+      //   }function(response,status){
+          
+      //   })
+      // }
+
     }
 
 
-  $(window).bind('scroll', function() {
-     if ($(window).scrollTop() > 0) {
-         $('#first').fadeOut();
-     }
-     else {
-         $('#first').fadeIn();
-     }
-  });
+  // $(window).bind('scroll', function() {
+  //    if ($(window).scrollTop() > 600) {
+  //        $('#first').fadeOut();
+  //    }
+  //    else {
+  //        $('#first').fadeIn();
+  //    }
+  // });
 
   setTimeout(function(){
     $("#myModal").modal('show')
