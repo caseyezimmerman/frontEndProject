b501b8a662517d7ad2318c75cf121ed945590b16
chris-malloy <chrismalloymusic@gmail.com>
2017-10-17 10:22:47 -0400

    add comments

--- runaround.js
+++ runaround.js
@@ -1,60 +1,43 @@
-$(document).ready(function(){
-	// var mapURL = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA9O_Kfl3FvK2OwNOA8qrLKpJ7wVTycGms`
-	var mapURL = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`
-	var map;
-
-
-
-	 var x = $.ajax({
-      type: 'GET',
-      url: mapURL,
-      dataType: 'jsonp',
-      jsonpCallback: 'initMap',
-      async: false, // this is by default false, so not need to mention
-      crossDomain: true // tell the browser to allow cross domain calls.
+$(document).ready(function() {
+    // var mapURL = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA9O_Kfl3FvK2OwNOA8qrLKpJ7wVTycGms`
+    var mapURL = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`
+    var map;
+    var x = $.ajax({
+        type: 'GET',
+        url: mapURL,
+        dataType: 'jsonp',
+        jsonpCallback: 'initMap',
+        async: false, // this is by default false, so not need to mention
+        crossDomain: true // tell the browser to allow cross domain calls.
     });
+    console.log(x)
+    console.log(mapURL)
+});
 
-	 console.log(x)
-	 console.log(mapURL)
-  });
+function initMap() {
+    var uluru = {
+        lat: 33.9304,
+        lng: -84.3733
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: uluru
+    });
+    console.log(map)
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
 
-  
- function initMap() {
-        var uluru = {
-            lat: 33.9304,
-            lng: -84.3733
-        };
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 14,
-            center: uluru
-        });
-        console.log(map)
-        var marker = new google.maps.Marker({
-            position: uluru,
-            map: map
-        });
+$(window).bind('scroll', function() {
+    if ($(window).scrollTop() > 600) {
+        $('#first').fadeOut();
+        // } else if ($(window.scrollTop() < 500)) {
+        // $('#first').fadeIn();
     }
+});
 
-
-  $(window).bind('scroll', function() {
-     if ($(window).scrollTop() > 0) {
-         $('#first').fadeOut();
-     }
-     else {
-         $('#first').fadeIn();
-     }
-  });
-
-  setTimeout(function(){
+setTimeout(function() {
     $("#myModal").modal('show')
-  }, 1000)
-
-
-
-
-
-
-
-
-
-
+}, 1000)
