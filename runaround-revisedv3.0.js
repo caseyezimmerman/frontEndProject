b07a055e65ae49331c0f5f0f5d3b3808206ba7ff
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-10-24 10:30:43 -0400

    no message

--- runaround-revisedv3.0.js
+++ runaround-revisedv3.0.js
@@ -1,58 +1,50 @@
 // GLOBAL Vars/stuff
 var mapURL = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`
-var roadURL = `https://roads.googleapis.com/v1/snapToRoads?path=60.170880,24.942795|60.170879,24.942796|60.170877,24.942796&key=${apiKey}`
-var userLocation;
+
 var ajaxRequest = $.ajax({
     type: 'GET',
     url: mapURL,
-    url2: roadURL,
     dataType: 'jsonp',
     jsonpCallback: 'initMap',
     async: false, // this is by default false, so not need to mention
     crossDomain: true // tell the browser to allow cross domain calls.
 });
 var userLocationLatLng;
-var init_lat;
-var init_lng;
-var range;
-var geocoder;
+var userLocation;
+var init_lat; ////userlocation.lat
+var init_lng; ///userlocation.lng
+var range; ///used to increase radius of path-path is getting longer
+var geocoder; ///turns the string of userlocation to usebale coordinates
 var marker;
-var marker2;
-var marker3;
 var map;
-var directionsDisplay;
+var directionsDisplay; 
 var directionsService;
 var stepDisplay;
 var markerArray = []
-var markerArray2 = []
-var markerArray3 = []
 var lat_lng;
 var latArray = []
 var lngArray = []
-var latArray2 = []
-var lngArray2 = []
-var latArray3 = []
-var lngArray3 = []
+
 $(document).ready(function() {
     $('.run-form').submit(function(event) {
         event.preventDefault();
-        $("#instructions").show()
         reset()
-        var address = document.getElementById("location").value;
-        var x = document.getElementById("miles").selectedIndex;
-        var distance = document.getElementsByTagName("option")[x].innerHTML;
-        console.log(distance)
+        $("#instructions").show()
+        userLocation = document.getElementById("location").value;
+        var indexValue = document.getElementById("miles").selectedIndex; ////for drop down menu
+        var distance = document.getElementsByTagName("option")[indexValue].innerHTML; ////for drop down menu
+        // console.log(distance)
         if (distance == "1-3 miles") {
-            range = .011
+            range = .007
         } else if (distance == "3-5 miles") {
-            range = .013
+            range = .01
         } else if (distance == "5-7 miles") {
-            range = .015
+            range = .012
         } else if (distance == "7-10 miles") {
-            range = .017
+            range = .013
         }
         geocoder = new google.maps.Geocoder();
-        geocoder.geocode({ 'address': address }, function(results, status) {
+        geocoder.geocode({ 'address': userLocation }, function(results, status) {
             if (status === 'OK') {
                 userLocationLatLng = {
                     lat: results[0].geometry.location.lat(),
@@ -67,10 +59,11 @@ $(document).ready(function() {
     });
 });
 
-function initMap(coordLocation = {
-    lat: 33.989073,
-    lng: -84.507361
-}) {
+function initMap(coordLocation = {   /////hard coded coordinates on map load
+                                    lat: 33.989073,
+                                    lng: -84.507361
+                                    }) 
+                                {
     initialize(coordLocation)
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
@@ -83,11 +76,11 @@ function initMap(coordLocation = {
     directionsService = new google.maps.DirectionsService; // calculates directions
     directionsDisplay = new google.maps.DirectionsRenderer({ map: map }); // renders the directions to the screen
     stepDisplay = new google.maps.InfoWindow; // do we need this?
-    directionsDisplay.setPanel(document.getElementById('instructions')); // sets the instruction panel
+    directionsDisplay.setPanel(document.getElementById('instructions')); // puts instructions on screen at HTML
 }
 
-function initialize(userLocation) {
-    markerArray = []
+function initialize(userLocation) { ///places one marker based on userlocation input
+    markerArray = [] ///empty the array on initialize
     init_lat = userLocation.lat
     console.log(init_lat)
     init_lng = userLocation.lng
@@ -95,38 +88,33 @@ function initialize(userLocation) {
     markerArray.push(marker);
     latArray.push(init_lat_lng.lat()) /////push lat of our initial point
     lngArray.push(init_lat_lng.lng()) /////push lng of our initial point
-    var myOptions = {
-        zoom: 13,
-        center: init_lat_lng,
-        mapTypeId: 'roadmap' ///////or this one
-    }
 }
 
-function findCoordinates(lat, lng, range) {
+function findCoordinates(lat, lng, range) { ////uses triangles to find extra points
     var numOfPoints = 6;
     var degreesPerPoint = -5 / numOfPoints;
     var x2;
     var y2;
     var currentAngle = 45;
-    // map = map;
     for (let i = 0; i <= numOfPoints; i++) {
         x2 = Math.cos(currentAngle) * range;
         y2 = Math.sin(currentAngle) * range;
         newLat = lat + x2;
         newLng = lng + y2;
-        lat_lng = new google.maps.LatLng(newLat, newLng);
-        marker = new google.maps.Marker({
+        lat_lng = new google.maps.LatLng(newLat, newLng); ///makes new points on the map
+        marker = new google.maps.Marker({ ////places marker on the map of those new points
             position: lat_lng,
             map: map,
         });
         latArray.push(lat_lng.lat()); ////push lats of points we just looped through and placed on map
         lngArray.push(lat_lng.lng());
         markerArray.push(marker);
-        currentAngle += degreesPerPoint;
+        currentAngle += degreesPerPoint; 
     }
 };
 
 function calculateAndDislayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map, userLocation) {
+    //////connects our waypoints
     for (let i = 0; i < markerArray.length; i++) {
         markerArray[i].setMap(null);
     }
@@ -178,8 +166,7 @@ function calculateAndDislayRoute(directionsDisplay, directionsService, markerArr
         avoidHighways: true,
     }, (response, status) => {
         if (status === "OK") {
-            console.log(map)
-            document.getElementById('warning-panel').innerHTML = '<b>' + response.routes[0].warnings + '</b>';
+            // console.log(map)
             directionsDisplay.setMap(map);
             directionsDisplay.setDirections(response);
         } else {
@@ -199,12 +186,9 @@ function reset() {
         position: lat_lng,
         map: map
     })
-    directionsService = new google.maps.DirectionsService;
-    directionsDisplay = new google.maps.DirectionsRenderer({ map: null });
+    directionsService = new google.maps.DirectionsService; ///calculats the directions
+    directionsDisplay = new google.maps.DirectionsRenderer({ map: null }); ///renders the directions
     stepDisplay = new google.maps.InfoWindow;
-    // directionsDisplay.setPanel(document.getElementById('instructions'));
     directionsDisplay.setPanel(null);
     document.getElementById("instructions").innerHTML = "";
 }
-var myRoute;
-var legsArray = []
